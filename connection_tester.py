e5529c3661e8ea23a953ad06489ceeb7dcbad67b
Sebastian Hietsch <hietschseb@outlook.com>
2017-09-08 16:32:26 +0200

    Refactor code and make available for windows

--- connection_tester.py
+++ connection_tester.py
@@ -5,60 +5,64 @@ import os
 import threading
 
 def run():
-        inst=mainThread()
-        inst.daemon=True
-        inst.start()
+	inst=mainThread()
+	inst.daemon=True
+	inst.start()
 
-        input()
+	input()
 
 class mainThread(threading.Thread):
-        def __init__(self):
-                threading.Thread.__init__(self)
-        def run(self):
-                connectionTester()
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+		connectionTester()
 
 def connectionTester():
-        global start_time
-        start_time=time.time()
+	global start_time
+	start_time=time.time()
 
-        global successful_attempts
-        successful_attempts=0
-        global failures
-        failures=0
-        global consecutive_success
-        consecutive_success=0
-        global best_consec
-        best_consec=0
+	global successes
+	successes=0
 
-        while True:
-                try:
-                        s=socket.socket()
-                        s.connect(("google.com",80))
+	global failures
+	failures=0
 
-                        successful_attempts+=1
-                        consecutive_success+=1
+	global consecutive_success
+	consecutive_success=0
 
-                        if consecutive_success>best_consec:
-                                best_consec=consecutive_success
+	global best_consec
+	best_consec=0
+
+	while True:
+		try:
+			s=socket.socket()
+			s.connect(("google.com",80))
+
+			successes+=1
+			consecutive_success+=1
+
+			if consecutive_success>best_consec:
+				best_consec=consecutive_success
 
 
-                except:
-                        failures+=1
-                        consecutive_success=0
+		except:
+			failures+=1
+			consecutive_success=0
 
-                os.system("clear")
-                display()
-                time.sleep(1)
+		display()
+		time.sleep(1)
 
 def display():
-        total=successful_attempts+failures
+	total=successes+failures
 
-        print("Internet connection tester\n" + "-"*26 + "\n")
-        print("Total attempts:",total)
-        print("Successes:     ",successful_attempts,round(successful_attempts/total*100,4),"%")
-        print("Failures:      ",failures,round(failures/total*100,4),"%")
-        print("Best:          ",best_consec)
-        print()
-        print("Running for:   ",round(time.time()-start_time,1),"seconds")
+	os.system('cls' if os.name == 'nt' else 'clear')
+	
+	print("Internet connection tester\n" + "-"*26 + "\n")
+	print("Total attempts:",total)
+	print("Successes:     ",successes,round(successes/total*100,4),"%")
+	print("Failures:      ",failures,round(failures/total*100,4),"%")
+	print("Best:          ",best_consec)
+	print()
+	print("Running for:   ",round(time.time()-start_time,1),"seconds")
 
 run()
