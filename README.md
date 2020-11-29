3597de445e41b56f47237d483cd802ce51ce40a2
Sebastian Hietsch <hietschseb@oulook.com>
2020-11-29 22:22:54 +0100

    Save data to file and add speedtest every 15 minutes

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # README #
 
-	This program can be run on your Linux PC or server using Python 3.
-	In its current configuration, it tries to connect to Google's servers once every second to test if the internet connection is working.
-	I use it to monitor Linux server uptime (assuming Google's server is up too of course). For this purpose I just have it run in the background, and check in on it every once in a while.
-	
+	This program can be run on your PC or server using Python 3.
+	In its current configuration, it tries to connect to Google's DNS server once every second to test if the internet connection is working.
+	I use it to monitor Linux server uptime (assuming Google's server is up too of course). For this purpose I have it run in the background, and check in on it every once in a while.
+
 	To quit, press return.
 	If you want to add parameters, run:
 	filename.py successes failures best seconds
+
+	speedtest-cli needs to be installed for this to work.
+
+	All results are saved to results.txt and results_speedtest.txt respectively
--- connection_tester.py
+++ connection_tester.py
@@ -19,6 +19,10 @@ class mainThread(threading.Thread):
 		connectionTester()
 
 def connectionTester():
+	if not os.path.exists("results_speedtest.txt"):
+		with open("results_speedtest.txt","w+") as file:
+			file.write("")
+
 	global start_time
 	start_time=time.time()
 
@@ -35,11 +39,14 @@ def connectionTester():
 	best_consec=0
 
 	parse_arguments()
-	
+
 	while True:
+		if (successes + failures)%900 == 0:
+			speedtestThread().start()
+
 		try:
 			s=socket.socket()
-			s.connect(("google.com",80))
+			s.connect(("8.8.8.8",53))
 
 			successes+=1
 			consecutive_success+=1
@@ -53,52 +60,87 @@ def connectionTester():
 			consecutive_success=0
 
 		display()
-		time.sleep(1)
+		time.sleep(0.95)
 
-		
+
+class speedtestThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+		#try:
+		import speedtest
+		spdt = speedtest.Speedtest()
+		spdt.get_best_server()
+		spdt.download()
+		spdt.upload()
+		results = spdt.results.dict()
+		txt = ""
+		txt += "{0}, host: {1}, {2}".format(results["timestamp"][:19].replace("T"," "),results["server"]["sponsor"], results["server"]["name"])
+		txt += "\n{0} ms ping".format(round(results["ping"]))
+		txt += "\n{0} mbps down".format(round(results["download"]/1000000,1))
+		txt += "\n{0} mbps up".format(round(results["upload"]/1000000,1))
+		txt += "\n\n"
+		#except:
+		#	txt = "Speedtest error\n\n"
+
+		try:
+			with open("results_speedtest.txt","r+") as file:
+				f = file.read()
+				file.write(f + txt)
+		except:
+			pass
+
 def parse_arguments():
 	args=sys.argv
 	l=len(args)-1
-	
+
 	if l>=1: #L==one
 		try:
 			global successes
 			successes=int(args[1])
 		except ValueError:
 			pass
-			
+
 	if l>=2:
 		try:
 			global failures
 			failures=int(args[2])
 		except ValueError:
 			pass
-			
+
 	if l>=3:
 		try:
 			global best_consec
 			best_consec=int(args[3])
 		except ValueError:
 			pass
-			
+
 	if l>=4:
 		try:
 			global start_time
 			start_time-=float(args[4])
 		except ValueError:
 			pass
-		
+
 def display():
 	total=successes+failures
 
 	os.system('cls' if os.name == 'nt' else 'clear')
-	
-	print("Internet connection tester\n" + "-"*26 + "\n")
-	print("Total attempts:",total)
-	print("Successes:     ",successes,round(successes/total*100,4),"%")
-	print("Failures:      ",failures,round(failures/total*100,4),"%")
-	print("Best:          ",best_consec)
-	print()
-	print("Running for:   ",round(time.time()-start_time,1),"seconds")
+
+	txt = ""
+	txt += "Internet connection tester\n" + "-"*26 + "\n"
+	txt += "\nTotal attempts: " + str(total)
+	txt += "\nSuccesses:      " + str(successes) + " (" +str(round(successes/total*100,4)) + " %)"
+	txt += "\nFailures:       " + str(failures) + " (" +str(round(failures/total*100,4)) + " %)"
+	txt += "\nBest:           " + str(best_consec)
+	txt += "\n"
+	txt += "\nRunning for:    " + str(round(time.time()-start_time,1)) + " seconds"
+
+	print(txt)
+	try:
+		with open("results.txt", "w") as file:
+			file.write(txt)
+	except:
+		pass
 
 run()
